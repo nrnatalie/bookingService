71e19dde04a3391480359326f4157a56929819e4
Nathalie Russo <nrnatusia7@gmail.com>
2024-04-10 13:49:19 +0300

    add config

---
+++ src/main/java/de/aittr/g_27_bookingService/config/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package de.aittr.g_27_bookingService.config;
+
+import java.util.Collections;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+
+
+
+
+@Configuration
+public class SwaggerConfig {
+
+  @Bean
+  public Docket api() {
+    return new Docket(DocumentationType.OAS_30)
+        .select()
+        .apis(RequestHandlerSelectors.basePackage("de.aittr.g_27_bookingService.controllers"))
+        .paths(PathSelectors.any())
+        .build()
+        .apiInfo(apiInfo());
+  }
+
+  private ApiInfo apiInfo() {
+    return new ApiInfo(
+        "Booking Service API",
+        "API для управления бронированием отелей", // Описание  API
+        "1.0", // Версия вашего API
+        "Terms of service",
+        new Contact("John Doe", "https://www.example.com", "john.doe@example.com"),
+        "MIT License", // Лицензия вашего API
+        "https://www.example.com/license", // URL лицензии вашего API
+        Collections.emptyList());
+  }
+}
+
+
--- src/main/java/de/aittr/g_27_bookingService/config/WebMvcConfig.java
+++ src/main/java/de/aittr/g_27_bookingService/config/WebMvcConfig.java
@@ -1,4 +1,4 @@
-package config;
+package de.aittr.g_27_bookingService.config;
 
 
 

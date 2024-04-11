b3c799ba78bb5ca82cb11a4d57bb7ffdbb2097e6
Nathalie Russo <nrnatusia7@gmail.com>
2024-04-11 19:48:19 +0300

    add @Entity

--- src/main/java/de/aittr/g_27_bookingService/domain/JpaUser.java
+++ src/main/java/de/aittr/g_27_bookingService/domain/JpaUser.java
@@ -1,15 +1,25 @@
 package de.aittr.g_27_bookingService.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
 import java.util.Objects;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class JpaUser implements User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+  @Column(nullable = false, unique = true)
   private String email;
+  @Column(nullable = false)
   private String password;
 
 
   public JpaUser() {
   }
-
   public JpaUser(String email, String password) {
     this.email = email;
     this.password = password;
